529aa2f00d605b2f9400ed0c003907197f13679b
673342907 <673342907@qq.com>
2025-12-01 23:07:35 +0800

    更新 Next.js 配置以解决 404 问题

--- test-app/next.config.js
+++ test-app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // 确保输出正确
+  // 明确指定输出目录
+  distDir: '.next',
 }
 
 module.exports = nextConfig
